f92a738400dc7260850066477bd3a5145341b922
felixpolomsky <109291826+felixpolomsky@users.noreply.github.com>
2024-10-10 21:37:30 +0200

    Update internet_player.py

--- internet_player.py
+++ internet_player.py
@@ -2,10 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 import time
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(service=Service("/bin/chromedriver"), options=options)
 driver.maximize_window()
 driver.get("http://your_url")
 
