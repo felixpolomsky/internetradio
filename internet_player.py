bba770e28a49fc3fa54fc00411a9bbbc83893fe0
felixpolomsky <109291826+felixpolomsky@users.noreply.github.com>
2024-12-10 22:11:06 +0100

    Update internet_player.py

--- internet_player.py
+++ internet_player.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import time
 
 options = Options()
